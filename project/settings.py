dc44837bf7975c4d5cb4a9729809ca351ac84a10
choosooa <josuah5160@dgu.ac.kr>
2026-05-04 00:26:36 +0900

    과제4

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'main',
+    'accounts',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,6 @@ USE_TZ = False
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'project', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
